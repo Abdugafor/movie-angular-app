2fc650a7fac3b61a6b5b5b9f8e08951e5595a892
Abdugafor Desktop <gafarovabdugafur@gmail.com>
2023-07-16 18:52:15 +0500

    f

--- src/app/modules/browse/popular/popular.component.ts
+++ src/app/modules/browse/popular/popular.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/http.service';
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+
+
 
 @Component({
   selector: 'app-popular',
@@ -9,18 +12,29 @@ import { HttpService } from 'src/app/http.service';
 
 export class PopularComponent implements OnInit {
   movies: any = []
+  
 
   constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
-    this.httpService.getMovies()
-    .subscribe({
-      next: res => this.movies = res,
-      error: error => console.log('error'),
-      complete: () => {
-        this.movies = this.movies.results
-        console.log(this.movies)
-      }
-    })
+    // this.httpService.getMovies()
+    // .subscribe({
+    //   next: res => this.movies = res,
+    //   error: error => console.log('error'),
+    //   complete: () => {
+    //     this.movies = this.movies.results
+    //     console.log(this.movies)
+    //   }
+    // })
+    new Glide('.glide', {
+      type: 'carousel',
+      startAt: 0,
+      perView: 5,
+      bound: true,
+      peek: {
+        before: 100,
+        after: 100
+      } 
+    }).mount({ Controls, Breakpoints })
   }
 }
